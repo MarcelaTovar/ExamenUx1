9c86ebfb0f740f7392c7b62f5ad774e69608c624
Marcela Tovar <marcelatovar.flores@gmail.com>
2024-05-17 21:21:22 -0600

    Barra de botones

--- examenux/src/App.js
+++ examenux/src/App.js
@@ -1,14 +1,15 @@
 import './App.css';
 import './organismos/Barras/Barras'
 import Barras from './organismos/Barras/Barras';
+import BarraBotones from './molecules/BarraBotones/BarraBotones';
 
 function App() {
   return (
     <div className="App">
       <div class="background-barra">
         <Barras></Barras>
+        <BarraBotones></BarraBotones>
       </div>
-
     </div>
   );
 }
---
+++ examenux/src/molecules/BarraBotones/BarraBotones.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Boton from '../../atoms/Boton/Boton';
+import './BarraBotones.css';
+
+
+const BarraBotones = () =>{
+    return(
+    <div class="scrollable-container">
+        <Boton nombre="Todos"></Boton>
+        <Boton nombre="Música"></Boton>
+        <Boton nombre="Mixes"></Boton>
+        <Boton nombre="Gorillaz"></Boton>
+        <Boton nombre="Pop rock"></Boton>
+        <Boton nombre="Teen pop"></Boton>
+        <Boton nombre="Ahorros"></Boton>
+        <Boton nombre="Pop japonés"></Boton>
+        <Boton nombre="R&B contemporáneo"></Boton>
+        <Boton nombre="Drake"></Boton>
+        <Boton nombre="Videojuegos"></Boton>
+        <Boton nombre="Misterio"></Boton>
+        <Boton nombre="Taylor Swift"></Boton>
+        <Boton nombre="Star Wars"></Boton>
+    </div>
+    );
+}
+
+export default BarraBotones;
